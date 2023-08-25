d85b6c74740df3687671e7ff402bd42d88c5a135
faceless_fk <fardin.k.s.10.1@gmail.com>
2023-08-25 19:26:31 +0330

    add tools for better error handleing make connection with database and ...

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 require("dotenv").config();
 // async Error
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    res.send("test");
+const mongoCon = require("./db/connection");
+
+const productsRt = require("./routes/products");
+
+const errorHandler = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+    res.send('<a href="/api/v1/products">hello</a>');
 });
 
+app.use("/api/v1/products", productsRt);
+
+// products route
+app.use(errorHandler);
+app.use(notFound);
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}...`);
-});
+
+const start = async () => {
+    try {
+        await mongoCon(
+            process.env.MONGO_URI,
+            console.log("database connected ...")
+        );
+        app.listen(port, console.log(`server is listening to port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+};
+start();
