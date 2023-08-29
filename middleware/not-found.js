558659bd47fa6611b20e287e466cd8d52c26292f
Faceless_fk21 <fardin.k.s.10.1@gmail.com>
2023-08-29 14:10:20 +0330

    new path

--- middleware/not-found.js
+++
@@ -1,3 +0,0 @@
-const notFound = (req, res) => res.status(404).send("Route dose not exist");
-
-module.exports = notFound;
--- routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-const productCtrl = require("../controllers/products");
-
-Router.route("/").get(productCtrl.getAllProducts);
-Router.route("/static").get(productCtrl.getAllProductsStatic);
-
-module.exports = Router;
